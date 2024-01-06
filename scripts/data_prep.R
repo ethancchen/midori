cf082a0cf1f701f3471b2ee38d9998894648ba57
Joshua <jiashu.chen@berkeley.edu>
2024-01-06 08:22:49 -0800

    created scripts dir; moved notebooks to be inside scripts dir; uploaded `data_prep.R`

---
+++ scripts/data_prep.R
@@ -0,0 +1,21 @@
+# Load necessary libraries
+library(dplyr)
+library(stringr)
+
+# Read the CSV file with explicit encoding specification
+file_path <- "./data/initial_data_set.csv"
+init_data <- read.csv(file_path, stringsAsFactors = FALSE, fileEncoding = "UTF-8")
+
+# Process the data, removing short problem and solutions.
+result_data <- init_data %>%
+  mutate(solution = iconv(solution, to = "UTF-8", sub = "byte"),  # Convert to UTF-8
+         res_sent_cnt = sapply(str_extract_all(solution, "[^.!?]+[.!?]"), length),
+         r_char_cnt = sapply(solution, nchar),
+         p_char_cnt = sapply(problem, nchar)) %>%
+  filter((res_sent_cnt > 10 | r_char_cnt >= 250) & p_char_cnt > 42) %>%
+  select(c(problem, id, solution))
+
+view(result_data)
+
+# Write the processed data to a new CSV file
+write.csv(result_data, "result_data.csv", row.names = FALSE)
