b00fa1016519589031b579286dd3c3e7aad419f3
Rucha <97582675+RuePat@users.noreply.github.com>
2024-01-07 12:54:31 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,10 @@ import streamlit as st
 import pandas as pd
 import get_started as gs
 
+import evaluator as ev
+import business_zone as bz
+import choose_idea as ci
+
 
 def page_welcome():
     st.markdown("<h1 style='text-align: left; color: #808000;'>MIDORI</h1>", unsafe_allow_html=True)
@@ -13,17 +17,31 @@ def page_get_started():
     gs.get_started_page()
 
 def page_evaluator():
-    st.title("Evaluator Page")
-    st.write("Calculate scores here")
+
+    ev.page_evaluator()
+    # st.title("Evaluator Page")
+    # st.write("Calculate scores here")
+
+def page_choose_idea():
+    st.title("Pick the idea you wish to create a business model of")
+    ci.page_choose_idea()
 
 def page_business_zone():
-    st.title("Business Zone Page")
-    st.write("Lean Canvas Generated here")
+    # st.title("Business Zone Page")
+    # st.write("Lean Canvas Generated here")
+    bz.page_business_zone()
+
 
 def page_about():
-    st.title("Meet the Team")
+    st.title("Meet the authors")
 
 def main():
+
+    if 'api_key' not in st.session_state:
+        st.session_state["api_key"]= "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"
+
+
+
     correct_username = "user"
     correct_password = "password"
 
@@ -57,7 +75,7 @@ def main():
             st.experimental_rerun()
 
         st.sidebar.title("Navigation")
-        pages = ["Welcome", "Get started", "Evaluator", "Business Zone", "About"]
+        pages = ["Welcome", "Get started", "Evaluator","Choose Idea", "Business Zone", "About"]
 
         # Updating the menu selection and rerunning the script if the selection changes
         current_selection = st.session_state["menu_selection"]
@@ -74,6 +92,8 @@ def main():
                 page_get_started()
             case "Evaluator":
                 page_evaluator()
+            case "Choose Idea":
+                page_choose_idea()
             case "Business Zone":
                 page_business_zone()
             case "About":
