e0452ad1ea8a21bbe5e1a4e20fd6b258295e83b9
Rucha <97582675+RuePat@users.noreply.github.com>
2024-01-07 12:57:40 -0500

    Add files via upload

---
+++ webapp/lean_canvas/lean_canvas_prompts.py
@@ -0,0 +1,10 @@
+# Create prompts string variables for generating lean canvas.
+problem_summary = "Summarize the problem in under 450 characters."
+solution_summary = "Summarize the solution in under 200 characters."
+uniq_val_prop = "What's the unique value proposition for the provided solution? Answer in 1 sentence under 450 characters."
+key_metrics = "What are the key metrics for the provided solution? Answer in pargraph form under 200 characters."
+unfair_advtg = "What are some unfair advantages that other companies may affect the effectiveness of the solution? Answer in pargraph form under 200 characters."
+channels = "How can one provide this solution to customers? Answer in pargraph form under 200 characters."
+customer_seg = "Who are the target customers? Answer in one sentence under 450 characters."
+cost_struct = "What is the cost structure for this solution? Answer in one sentence under 470 characters."
+revenue_streams = "What are different types of revenue streams for this solution? Answer in pargraph form under 470 characters."
