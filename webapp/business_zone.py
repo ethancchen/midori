5ec1d25986544776b6388a26bbc4110ebc7b9280
Rucha <97582675+RuePat@users.noreply.github.com>
2024-01-07 12:55:53 -0500

    Add files via upload

---
+++ webapp/business_zone.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+from PIL import Image
+import lean_canvas.draw as d
+
+def page_business_zone():
+    st.title("Business Zone Page")
+    st.write("Lean Canvas Generated here")
+    img = d.draw()
+    st.image(img, caption='Uploaded Image.', use_column_width=True)
